c98435d1b3ececa6db45ed270d1b3834e09c1d1c
ellacee <ella.green.se@gmail.com>
2025-11-19 16:14:46 +0100

    added isotropic resampling

--- tristan_exp.py
+++ tristan_exp.py
@@ -1,6 +1,5 @@
 from utilities import (
     load_nrrd_mask,
-    ensure_continous_body,
     extract_centerline_skimage,
     extract_endpoint_and_bifurcation_coordinates,
     skeleton_to_sparse_graph,
@@ -13,7 +12,7 @@ from utilities import (
     create_distance_transform_from_mask,
     compute_branch_diameters_of_graph,
     compute_branch_lengths_of_graph,
-    merge_branch_metrics,
+    #merge_branch_metrics,
     make_directed_graph,
     determine_origin_node_from_diameter,
     traverse_graph_and_compute_angles,
@@ -26,7 +25,7 @@ import numpy as np
 
 # I'm loading the data here for a single file, it gets loaded into 3d numpy array, and
 # a seperate header dictionary with information about the data
-path = "data/batch_1/Normal_2.nrrd"
+path = "data/COURSEDATA/ArkivSEG/Normal_5.nrrd"
 binary_mask, header = load_nrrd_mask(path, verbose=True)
 
 # Here I'm extracting the spacing/direction information from the header data
@@ -34,20 +33,23 @@ binary_mask, header = load_nrrd_mask(path, verbose=True)
 # to actual units
 spacing_info = tuple(np.diag(header['space directions']))
 
+# Upsamples the voxel spacing so that spacing is the same for all axes
+binary_mask, spacing_info = resample_to_isotropic(binary_mask, spacing_info)
+
 
 # So far the preprocessings can be an optional upsample and a gaussian filter for smoothing.
 binary_mask = preprocess_binary_mask(binary_mask, upsample_factor=1)
 
-# This function will check the mask to see if it is one continous body, a boolean flag
-# indicates its continous while the labelled_bodies array is an array of the same shape
-# as binary_mask, but each unique continous body has a unique label, ie 0 = background, 1 = body 1, 2 = body 2 etc..
-is_continous, labelled_bodies = ensure_continous_body(binary_mask)
+# This function will check the mask to see if it is one continuous body, a boolean flag
+# indicates its continuous while the labelled_bodies array is an array of the same shape
+# as binary_mask, but each unique continuous body has a unique label, ie 0 = background, 1 = body 1, 2 = body 2 etc..
+is_continuous, labelled_bodies = ensure_continuous_body(binary_mask)
 
-if is_continous:
-    print("is continous")
+if is_continuous:
+    print("is continuous")
     original_one_sided_mask = (labelled_bodies == 1).astype(np.uint8)
 else:
-    print("it ain't continous")
+    print("it ain't continuous")
     sorted_bodies = sort_labelled_bodies_by_size(labelled_bodies)
     original_one_sided_mask = sorted_bodies[1]
     # original_one_sided_mask = (labelled_bodies == 1).astype(np.uint8)
@@ -64,12 +66,12 @@ skeleton_binary_mask = extract_centerline_skimage(original_one_sided_mask)
 
 skeleton_binary_mask_no_processing = skeleton_binary_mask
 
-# is_continous, labelled_bodies = ensure_continous_body(centerline_binary_mask)
+# is_continuous, labelled_bodies = ensure_continuous_body(centerline_binary_mask)
 #
-# if is_continous:
-#     print("is continous")
+# if is_continuous:
+#     print("is continuous")
 # else:
-#     print("it ain't continous")
+#     print("it ain't continuous")
 #     print(np.unique(labelled_bodies))
 
 # create_projection_view(centerline_binary_mask)
--- utilities/__init__.py
+++ utilities/__init__.py
@@ -10,14 +10,13 @@ This package provides utility functions for:
 """
 
 from .input_output import load_nrrd_mask
-from .centerline_utils import ensure_continous_body, extract_centerline_skimage
+from .centerline_utils import ensure_continuous_body, extract_centerline_skimage
 from .bifurcation_utils import extract_endpoint_and_bifurcation_coordinates, remove_redundant_bifurcation_clusters, remove_sharp_bend_bifurcations
 from .graph_utils import skeleton_to_sparse_graph, find_connected_voxels, skeleton_to_dense_graph, dense_graph_to_skeleton, make_directed_graph 
-from .preprocessing_utils import preprocess_binary_mask, sort_labelled_bodies_by_size
 from .diameter_utils import create_distance_transform_from_mask, compute_average_diameter_of_branch, compute_branch_diameters_of_graph, determine_origin_node_from_diameter
 from .distance_utils import compute_branch_path_length, compute_branch_lengths_of_graph
 from .trigonometric_utils import move_along_centerline, fit_bifurcation_plane, compute_inflow_angle, compute_bifurcation_angles, compute_angles_at_bifurcation, traverse_graph_and_compute_angles
-from .general_utils import merge_branch_metrics
+#from .general_utils import merge_branch_metrics
 
 __all__ = [
     'load_nrrd_mask',
@@ -38,7 +37,7 @@ __all__ = [
     'compute_branch_diameters_of_graph',
     'compute_branch_path_length',
     'compute_branch_lengths_of_graph',
-    'merge_branch_metrics',
+    #'merge_branch_metrics',
     'determine_origin_node_from_diameter',
     'move_along_centerline',
     'fit_bifurcation_plane',
@@ -46,4 +45,5 @@ __all__ = [
     'compute_bifurcation_angles',
     'compute_angles_at_bifurcation',
     'traverse_graph_and_compute_angles',
+    'resample_to_isotropic',
 ]

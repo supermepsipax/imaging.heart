021ac43b4732fdf04c5297312a535d2dc677aa18
Medtech-code <prarthanaduraisamy2605@gmail.com>
2025-11-29 20:44:55 +0100

    diameter extraction function added

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -13,7 +13,7 @@ from .centerline_utils import ensure_continous_body, extract_centerline_skimage
 from .bifurcation_utils import extract_endpoint_and_bifurcation_coordinates, remove_redundant_bifurcation_clusters, remove_sharp_bend_bifurcations
 from .graph_utils import skeleton_to_sparse_graph, find_connected_voxels, skeleton_to_dense_graph, dense_graph_to_skeleton
 from .preprocessing_utils import preprocess_binary_mask, sort_labelled_bodies_by_size
-from .diameter_utils import create_distance_transform_from_mask, compute_average_diameter_of_branch, compute_branch_diameters_of_graph
+from .diameter_utils import create_distance_transform_from_mask, compute_average_diameter_of_branch, compute_branch_diameters_of_graph,diameter_profile,summarize_profile
 from .distance_utils import compute_branch_path_length, compute_branch_lengths_of_graph
 from .general_utils import merge_branch_metrics
 
@@ -33,6 +33,8 @@ __all__ = [
     'create_distance_transform_from_mask',
     'compute_average_diameter_of_branch',
     'compute_branch_diameters_of_graph',
+    'diameter_profile',
+    'summarize_profile',
     'compute_branch_path_length',
     'compute_branch_lengths_of_graph',
     'merge_branch_metrics',
